61d5deb900996246e775d712b9863635a23b5725
marcos <marcos@marcos>
2017-05-04 18:47:39 -0300

    Create

---
+++ NSGAIII.m
@@ -0,0 +1,8 @@
+clc;
+
+global N; %Número de indivíduos
+
+N = 20;
+
+
+population = generate_initial_population (N); 
---
+++ generate_initial_population.m
@@ -0,0 +1,11 @@
+function [ population_r ] = generate_initial_population( N)
+
+    population_r(1,N) = Individual();
+    
+     for i=1 : N
+            n = randi(34)-17;
+            population_r(i).x = n;
+     end
+     
+     
+end 
